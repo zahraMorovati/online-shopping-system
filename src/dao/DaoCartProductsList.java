8cf8de8aca08293789d7ef343d00b7943639128d
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-10-19 07:33:47 +0330

    add save and delete method

--- src/dao/DaoCartProductsList.java
+++ src/dao/DaoCartProductsList.java
@@ -1,5 +1,8 @@
 package dao;
 
+import model.Cart;
+import model.CartProductsList;
+
 import java.sql.*;
 
 public class DaoCartProductsList extends Dao{
@@ -30,6 +33,33 @@ public class DaoCartProductsList extends Dao{
                 "    FOREIGN KEY (id_user) REFERENCES user(id)" +
                 "    FOREIGN KEY (id_product) REFERENCES product(id))");
     }
+
+    public int save(CartProductsList c) throws SQLException {
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
+            String sqlQuery = String.format("INSERT INTO cartProductsList" +
+                            " (id_cart,id_user,id_product) " +
+                            "VALUES ('%d','%d','%d')",
+                    c.getCartId(),c.getUserId(),c.getProductId());
+            int i = statement.executeUpdate(sqlQuery);
+            return i;
+        } else {
+            return 0;
+        }
+    }
+
+    public int delete(int cartId) throws SQLException {
+        if(getConnection() != null){
+            Statement statement = getConnection().createStatement();
+            String sqlQuery = String.format("delete from cartProductsList where id_cart='%d'",cartId);
+            int i = statement.executeUpdate(sqlQuery);
+            return i;
+        }else {
+            return 0;
+        }
+    }
+
+
 
 
 }
