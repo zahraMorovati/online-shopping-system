5677f8823bf959a353354bde8a41fe63bff2164e
zahra morovati <90718563+zahraMorovati@users.noreply.github.com>
2021-10-19 06:31:05 +0330

    add findAll and findByID method

--- src/dao/DaoCart.java
+++ src/dao/DaoCart.java
@@ -1,8 +1,13 @@
 package dao;
 
 import model.Cart;
+import model.Product;
+import model.enumation.ProductGroup;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DaoCart extends Dao {
 
@@ -23,26 +28,64 @@ public class DaoCart extends Dao {
         Statement statement = connection.createStatement();
         statement.executeUpdate("CREATE TABLE cart (" +
                 "    id INT NOT NULL  AUTO_INCREMENT," +
-                "    id_user INT," +
                 "    status boolean DEFAULT false," +
                 "    total_price DOUBLE ," +
                 "    products_count INT," +
-                "    PRIMARY KEY (id) ," +
-                "    FOREIGN KEY (id_user) REFERENCES user(id))");
+                "    PRIMARY KEY (id))");
     }
 
     public int save(Cart c) throws SQLException {
         if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("INSERT INTO cart" +
-                            " (id_user,status,total_price,products_count) " +
-                            "VALUES ('%d','%d','%f','%d')",
-                    c.getUser().getId(),c.getStatus(),c.getTotalPrice(),c.getProductsCount());
+                            " (status,total_price,products_count) " +
+                            "VALUES ('%d','%f','%d')",
+                    c.getStatus(),c.getTotalPrice(),c.getProductsCount());
             int i = statement.executeUpdate(sqlQuery);
             return i;
         } else {
             return 0;
         }
+    }
+
+    public List<Cart> findAllCarts() throws SQLException {
+
+        if (getConnection() != null) {
+            List<Cart> cartList = new ArrayList<>();
+            Statement statement = getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from cart");
+            while (resultSet.next()) {
+                int id=resultSet.getInt("id");
+                boolean status=resultSet.getBoolean("status");
+                double totalPrice=resultSet.getDouble("total_price");
+                int productsCount=resultSet.getInt("products_count");
+                Cart cart=new Cart(id,status,totalPrice,productsCount);
+                cartList.add(cart);
+            }
+            return cartList;
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    public Cart findCartByID(int cartID) throws SQLException {
+
+        if (getConnection() != null) {
+            Statement statement = getConnection().createStatement();
+            String sqlQuery=String.format("select * from cart where id='%d'",cartID);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while (resultSet.next()) {
+                int id=resultSet.getInt("id");
+                boolean status=resultSet.getBoolean("status");
+                double totalPrice=resultSet.getDouble("total_price");
+                int productsCount=resultSet.getInt("products_count");
+                Cart cart=new Cart(id,status,totalPrice,productsCount);
+                return cart;
+            }
+        } else {
+            return null;
+        }
+        return null;
     }
 
 
